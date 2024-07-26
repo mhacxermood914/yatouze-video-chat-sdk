d478f26b572919b0455ca36ee59968f2ed015a9e
mhacxermood914 <soultonew2@gmail.com>
2024-07-26 16:00:13 +0100

    feat: update

--- src/components/YatouzeVideoChat.tsx
+++ src/components/YatouzeVideoChat.tsx
@@ -13,9 +13,9 @@ export default function YatouzeVideoChat(){
     const iceServers:any= [
         { urls: "stun:stun.l.google.com:19302" },
         {
-            urls: 'turn:openrelay.metered.ca:80',
-            username: 'openrelayproject',
-            credentials: 'openrelayproject'
+            urls: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
         },
         { urls: "stun:stun.l.google.com:5349" },
         { urls: "stun:stun1.l.google.com:3478" },
