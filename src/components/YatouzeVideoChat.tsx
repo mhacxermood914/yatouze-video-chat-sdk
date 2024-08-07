59a9cff621be76ce53fc586bdb2b14bfe5322af2
mhacxermood914 <soultonew2@gmail.com>
2024-08-07 18:32:32 +0100

    feat: review ui interface

--- src/components/YatouzeVideoChat.tsx
+++ src/components/YatouzeVideoChat.tsx
@@ -4,223 +4,9 @@
 // /* eslint-disable @typescript-eslint/no-unused-vars */
 // /* eslint-disable react-hooks/exhaustive-deps */
 // /* eslint-disable @typescript-eslint/no-explicit-any */
-// import React, { useRef } from "react"
-// import socket from "../utils/socket"
-
-// export default function YatouzeVideoChat(){
-
-//     const localVideo: any = useRef(null)
-//     const remoteVideo: any = useRef(null)
-//     const uid:any = generateUid(12)
-//     let rtcPeerConnection: any, remoteStream:any, localStream:any;
-//     const iceServers:any= [
-//         { urls: "stun:stun.l.google.com:19302" },
-//         {
-//             urls: 'turn:numb.viagenie.ca',
-//             credential: 'muazkh',
-//             username: 'webrtc@live.com'
-//         },
-//         { urls: "stun:stun.l.google.com:5349" },
-//         // { urls: "stun:stun1.l.google.com:3478" },
-//         // { urls: "stun:stun1.l.google.com:5349" },
-//         // { urls: "stun:stun2.l.google.com:19302" },
-//         // { urls: "stun:stun2.l.google.com:5349" },
-//         // { urls: "stun:stun3.l.google.com:3478" }
-//     ]
-
-//     const setupDeviceAndRTC = async ()=>{
-//         const mediaStream = await navigator.mediaDevices.getUserMedia({
-//             video:true, audio: true
-//         })
-//         if(localVideo.current)localVideo.current.srcObject= mediaStream;
-
-//         localStream = mediaStream
-
-//         rtcPeerConnection =new RTCPeerConnection({iceServers})
-
-//         // rtcPeerConnection.getSenders().forEach((sender:any) => rtcPeerConnection.removeTrack(sender));
-
-//         localStream.getTracks().forEach((track:any)=>{
-//             rtcPeerConnection.addTrack(track, localStream)
-//         })
-
-//         rtcPeerConnection.onconnectionstatechange=(event:any)=>{
-//             if(event.target.connectionState === "disconnected" || event.target.connectionState === "failed"){
-//                 console.log({event})
-//                 handleDisconnection()
-//             }
-//         }
-
-//         rtcPeerConnection.onicecandidate = function(event:any){
-//             if(event.candidate){
-//                 console.log({event})
-//                 socket.emit("candidate", event.candidate);
-//             }
-//         }
-
-//         rtcPeerConnection.oniceconnectionstatechange  = function(){
-//             console.log('ICE Connection State:', rtcPeerConnection.iceConnectionState);
-//         }
-
-//         rtcPeerConnection.onicecandidateerror = function(event:any) {
-//             console.error('ICE Candidate Error:', event);
-//         };
-
-//         rtcPeerConnection.ontrack = (ev:any) => {
-//             console.log({ev:ev.streams[0]})
-//         }
-
-//         socket.emit('join', {
-//             room: "main",
-//             name:generateUid(14)
-//         })
-
-//         // createOffer()
-//     }
-
-//     async function handleDisconnection() {
-//         console.log('Connection lost. Attempting to renegotiate...');
-      
-//         // Close the existing connection
-//         if (rtcPeerConnection) {
-//             rtcPeerConnection.close();
-//             rtcPeerConnection = null;
-//         }
-      
-//         // Recreate the peer connection and add local tracks
-//         await createPeerConnection('');
-      
-//         // createOffer()
-//       }
-
-//     const createPeerConnection = async (memberId:any)=>{
-//         console.log({memberId})
-//         remoteStream = new MediaStream()
-//         remoteVideo.current.srcObject = remoteStream
-//         if(rtcPeerConnection){
-//             rtcPeerConnection.ontrack = (ev:any) => {
-//                 console.log({ev:ev.streams[0]})
-//                 ev.streams[0].getTracks().forEach((track:any)=>{
-//                     remoteStream.addTrack(track)
-//                 })
-                   
-//             };
-//         }
-        
-//     }
-
-//     const createOffer = async ()=>{
-//         createPeerConnection(uid)
-//         const offer:any = await rtcPeerConnection.createOffer({offerToReceiveVideo:true,iceRestart: true})
-//         rtcPeerConnection.setLocalDescription(offer)
-//         socket.emit('offer',{
-//             offer,
-//             memberId: uid
-//         })
-//     }
-
-//     const createAnswer = async (offer:any)=>{
-//         createPeerConnection(uid)
-//         await rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(offer))
-//         const answer = await rtcPeerConnection.createAnswer({offerToReceiveVideo:true})
-//         rtcPeerConnection.setLocalDescription(answer)
-//         socket.emit('answer',{
-//             answer
-//         })
-//     }
-
-//     const handleToggleCamera = async ()=>{
-//         const videoTrack:any = localStream.getTracks().find((tracks:any)=>tracks.kind === 'video')
-
-//         if(videoTrack.enabled){
-//             videoTrack.enabled = false
-//         }else{
-//             videoTrack.enabled = true
-//         }
-//     }
-
-//     const handleToggleMicro = async ()=>{
-//         const audioTrack:any = localStream.getTracks().find((tracks:any)=>tracks.kind === 'audio')
-
-//         if(audioTrack.enabled){
-//             audioTrack.enabled = false
-//         }else{
-//             audioTrack.enabled = true
-//         }
-//     }
-
-
-//     React.useEffect(()=>{
-//         setupDeviceAndRTC()
-//     },[])
-
-//     React.useEffect(()=>{
-//         socket.connect()
-    
-//         socket.on('getOffer', (data)=>{
-//             console.log({data})
-//             createAnswer(data.offer)
-//         })
-
-//         socket.on('getAnswer',(data)=>{
-//             console.log({data})
-//             rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(data.answer))
-//         })
-
-
-//         socket.on('memberJoined', (data)=>{
-//             if(data.is_new_user){
-//                 // pour gerer la renegociation en cas de deconnexion (cloture d'onglet ou reload de page) d'un des peers
-//                 // actuellement je me base sur des event de la webRTC , on pourrais dans un futur proche gerer avec les websockets..
-//                 // apres plusieurs teste les websockets seront beaucoup plus efficace , car l'emission de l'event disconnected par webRTC viens
-//                 // parfois tardivement
-
-//                 if(!rtcPeerConnection){
-//                     setupDeviceAndRTC()
-//                     createPeerConnection('')
-//                 }
-//                 createOffer()
-//             }
-//         })
-
-//         socket.on("getCandidate", (candidate) => {
-//             if(rtcPeerConnection){
-//                 rtcPeerConnection.addIceCandidate(candidate)
-//             }
-        
-//         });
-
-//         // return ()=> socket.disconnect()
-//     },[])
-
-//     return (
-//         <>
-//             <h1> Yatouze Video Chat</h1>
-//             <div id="videos">
-//                 <video className="vid-player" ref={localVideo} autoPlay playsInline></video>
-//                 <video className="vid-player" ref={remoteVideo} autoPlay playsInline></video>
-//             </div>
-//             <div>
-//                 <button id="join" className="btn" onClick={()=>createOffer()}>Click to join</button>
-//                 <button onClick={handleToggleCamera}>Toogle Camera</button>
-//                 {/*<button>End Call</button>*/}
-//                 <button onClick={handleToggleMicro}>Toogle Audio</button>
-//             </div>
-//         </>
-//     )
-// }
-
-// function generateUid(length: any) {
-//     const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-//     let result = '';
-//     for (let i = 0; i < length; i++) {
-//         const randomIndex = Math.floor(Math.random() * characters.length);
-//         result += characters[randomIndex];
-//     }
-//     return result;
-// }
 
 import React, {useState, useEffect, useRef} from 'react'
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import socket from '../utils/socket'
 import { Device } from 'mediasoup-client';
 
@@ -261,18 +47,26 @@ export default function YatouzeVideoChat(){
     }
 
     const addElements= (id: string,  stream: any)=>{
-        const newElem = document.createElement('div')
-        newElem.setAttribute('id', `div-${id}`)
-        // newElem.setAttribute('class', 'remoteVideo')
-        newElem.innerHTML = "<video id="+id+" autoplay class='video'></video>"
-        if(videoChatContainer?.current)videoChatContainer.current.appendChild(newElem)
-
-        const vidElemnt:any = document.getElementById(id)
-
-
-        console.log({vidElemnt})
+        if(id !== 'localVideo'){
+            const newElem = document.createElement('div')
+            newElem.setAttribute('id', `div-${id}`)
+            newElem.setAttribute('className', `w-[8rem] h-[8rem]`)
+            // newElem.setAttribute('class', 'remoteVideo')
+            newElem.innerHTML = "<video id="+id+" autoplay class='video'></video>"
+            if(videoChatContainer?.current)videoChatContainer.current.appendChild(newElem)
     
-        if(vidElemnt)vidElemnt.srcObject = stream
+            const vidElemnt:any = document.getElementById(id)
+    
+    
+            console.log({vidElemnt})
+        
+            if(vidElemnt)vidElemnt.srcObject = stream
+        }else{
+            const htmlElement:any = document.getElementById('localVideo')
+
+            htmlElement.srcObject = stream
+
+        }
         
     }
 
@@ -476,8 +270,34 @@ export default function YatouzeVideoChat(){
     },[])
 
     return (
-        <div ref={videoChatContainer} className='grid grid-cols-5'>
+        <div  className='flex items-center h-screen'>
+            <div className='w-3/4 rounded-xl mx-auto h-[35rem] shadow-xl'>
+                <div className='p-6'>
+                    <div className='flex items-center justify-between space-x-4'>
+                        <div className='p-2 shadow-md w-[2rem] rounded-lg h-[2rem] flex justify-center items-center cursor-pointer'>
+                            <IoIosArrowBack />
+                        </div>
+                        {/* new coming users */}
+                        <div className='flex space-x-4 justify-between items-center flex-1  '>
+                            <div className='flex flex-1 space-x-4  overflow-x-auto'>
+                                <div className='h-[11rem] w-full bg-gray-400 rounded-lg'></div>
+                                <div className='h-[11rem] w-full bg-gray-400 rounded-lg'></div>
+                                <div className='h-[11rem] w-full bg-gray-400 rounded-lg'></div>
+                            </div>
+                                
+                        </div>
+                        <div className='p-2 shadow-md w-[2rem] rounded-lg h-[2rem] flex justify-center items-center cursor-pointer'>
+                            <IoIosArrowForward />
+                        </div>
+                    </div>
+                </div>
+                <div className='px-[4.5rem]'>
+                    <div  className='bg-gray-300 h-[19rem] w-full rounded-lg overflow-clip'>
+                        <video id='localVideo' className='w-full h-full object-cover' autoPlay playsInline></video>
+                    </div>
+                </div>
 
+            </div>
         </div>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Load environment variables from .env
+dotenv.config();
+
+export default defineConfig(({ mode }) => {
+  const { APP_URL } = process.env;
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    define: {
+      // Pass environment variables to the client-side code
+      'process.env': {
+        APP_URL: JSON.stringify(APP_URL),
+      },
+    },
+  };
+});
